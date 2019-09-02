2f6d854c22647a5675c70a3214ee703d3d5146c5
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2019-09-02 09:19:19 +0200

    incremented version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
 
 setup(name='xmlplot',
-      version='0.9.12',
+      version='0.9.13',
       description=' --- update - ',
       long_description=readme(),
       url='http://github.com/BoldingBruggeman/xmlplot',
@@ -28,5 +28,5 @@ setup(name='xmlplot',
       packages=['xmlplot', 'xmlplot/data'],
       package_data={'xmlplot': ['icons/*', 'schemas/*/*']}, 
       include_package_data=True,
-      install_requires=['xmlstore>=0.9.6', 'numpy', 'matplotlib'],
+      install_requires=['xmlstore>=0.9.7', 'numpy', 'matplotlib'],
       zip_safe=False)
