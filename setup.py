db05d107f5b52a7235dbbdaa8b9b418b0479298d
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2023-03-27 08:50:07 +0100

    boosted version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
 
 setup(name='xmlplot',
-      version='0.9.25',
+      version='0.9.26',
       description=' --- update - ',
       long_description=readme(),
       url='http://github.com/BoldingBruggeman/xmlplot',
